b3daaa097c54696424cc38c9bb6070771de9ee70
TianJin <SUJINXU@tianjindeimac.local>
2016-11-10 14:25:22 +0800

    Initial Commit

---
+++ CAGradienDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAGradienDemo
+//
+//  Created by 苏金旭 on 16/11/10.
+//  Copyright © 2016年 天津大象. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAGradienDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAGradienDemo
+//
+//  Created by 苏金旭 on 16/11/10.
+//  Copyright © 2016年 天津大象. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
